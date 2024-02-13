d63c4774a6131f6851009a589915af31c4a9b7e3
camcoder337 <camcoder337@gmail.com>
2024-02-13 10:43:10 +0100

    Hard example Page

---
+++ lib/levels/hard_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../riverpod.dart';
+
+class HardPage extends ConsumerWidget {
+  const HardPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
+        title: const Text('Hard Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              ref.watch(riverpodHardLevel).counter.toString(),
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            ElevatedButton.icon(
+                onPressed: (){
+                  ref.read(riverpodHardLevel).addCounter();
+                },
+                icon: const Icon(Icons.add),
+                label: const Text('Add')
+            ),
+            ElevatedButton.icon(
+                onPressed: (){
+                  ref.read(riverpodHardLevel).removeCounter();
+                },
+                icon: const Icon(Icons.remove),
+                label: const Text('Remove')
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
